5bd28ebfbba4113a84fd3ef8e65812f0a3b15512
Grosss <artemenko,nickolai@gmail.com>
2017-04-10 13:08:05 +0200

    consoleui is added

---
+++ Task1.ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Task1.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookListStorage storage = new BookListStorage("BookList.txt");
+            Book book1 = new Book("first", "A", 2000, 15);
+            Book book2 = new Book("second", "B", 1996, 20);
+            var bookList = new List<Book>();
+            bookList.Add(book1);
+            bookList.Add(book2);
+            storage.WriteBooksToFile(bookList);
+            var bookL = (List<Book>)storage.ReadBooksFromFile();
+            foreach (var x in bookL)
+                Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
